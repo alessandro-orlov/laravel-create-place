9ea137c89ec714c23f96d52018f8f5d7d31b5824
Alessandro <alex.ixs@hotmail.com>
2020-08-31 20:56:32 +0200

    Esercizio completato

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,36 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Student;
 
 class StudentController extends Controller
 {
     public function index() {
-      $array_students = [
-        [
-          'nome' => 'Alessandro',
-          'cognome' => 'Rossi',
-          'voto' => 10.
-        ],
-        [
-          'nome' => 'Marco',
-          'cognome' => 'Verdi',
-          'voto' => 9.
-        ],
-        [
-          'nome' => 'Francesca',
-          'cognome' => 'Forte',
-          'voto' => 7.
-        ],
-        [
-          'nome' => 'Michele',
-          'cognome' => 'Bianchi',
-          'voto' => 4.
-        ],
-      ];
+      // $array_students = [
+      //   [
+      //     'name' => 'Alessandro',
+      //     'lastname' => 'Rossi',
+      //     'vote' => 10.
+      //   ],
+      //   [
+      //     'name' => 'Marco',
+      //     'lastname' => 'Verdi',
+      //     'vote' => 9.
+      //   ],
+      //   [
+      //     'name' => 'Francesca',
+      //     'lastname' => 'Forte',
+      //     'vote' => 7.
+      //   ],
+      //   [
+      //     'name' => 'Michele',
+      //     'lastname' => 'Bianchi',
+      //     'vote' => 4.
+      //   ],
+      // ];
 
-      return view('students', [
-          'teacher' => 'Alessandro Scolozzi',
-          'students' => $array_students,
-        ]);
-    }
+      $array_students = Student::all();
+
+      return view('students', compact('array_students'));
+
+      // return view('students', [
+      //     'teacher' => 'Alessandro Scolozzi',
+      //     'students' => $array_students,
+      //   ]);
+
+      }
 }
--- resources/views/students.blade.php
+++ resources/views/students.blade.php
@@ -4,12 +4,12 @@
   <div class="container">
     <main>
       <h1>Pagina Studenti</h1>
-      <h3>Teacher: {{ $teacher }}</h3>
-      <?php foreach ($students as $student) { ?>
+      {{-- <h3>Teacher: {{ $teacher }}</h3> --}}
+      <?php foreach ($array_students as $student) { ?>
         <ul>
-          <li>Nome: {{$student['nome']}}</li>
-          <li>Cognome: {{$student['cognome']}}</li>
-          <li>Voto: {{$student['voto']}}</li>
+          <li>Nome: {{$student['name']}}</li>
+          <li>Cognome: {{$student['lastname']}}</li>
+          <li>Voto: {{$student['vote']}}</li>
         </ul>
       <?php } ?>
     </main>
